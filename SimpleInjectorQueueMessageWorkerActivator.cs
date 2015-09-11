e745820b4e9226c7170f9a51a315e2f45a1e6213
matt <matt@nerdamigo.com>
2015-09-11 10:51:38 -0700

    initial commit

---
+++ SimpleInjectorQueueMessageWorkerActivator.cs
@@ -0,0 +1,25 @@
+﻿using NerdAmigo.Abstractions;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdAmigo.Common.SimpleInjector
+{
+    public class SimpleInjectorQueueMessageWorkerActivator : IQueueMessageWorkerActivator
+    {
+		private Container container;
+
+		public SimpleInjectorQueueMessageWorkerActivator(Container container)
+		{
+			this.container = container;
+		}
+
+		public IQueueMessageWorker<TQueueMessage> GetWorker<TQueueMessage>() where TQueueMessage : IQueueMessage
+		{
+			return this.container.GetInstance<IQueueMessageWorker<TQueueMessage>>();
+		}
+	}
+}
